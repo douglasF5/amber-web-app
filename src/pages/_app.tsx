4b2b6a1de47fb90f918092802c794f223aa5a925
Douglas Ferreira <dfaferreira46@gmail.com>
2022-07-17 00:57:24 -0300

    added sass and primitive styles to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import  '../styles/main.scss';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1>Hello, bitches!</h1>
+    <h1>Amber podcasts</h1>
   )
 }
