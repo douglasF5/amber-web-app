ba5e26da9d997fa27dcc4e980df6fda014221e90
Douglas Ferreira <dfaferreira46@gmail.com>
2022-07-21 22:21:14 -0300

    finised podcast listing on the UI

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,10 +1,10 @@
 import s from './styles.module.scss';
-import { Play } from '../Icons';
+import { Play, ChevronUpMini, AudioBars } from '../Icons';
 import Image from 'next/image';
 
 type ListItemProps = {
     data: string;
-    isExpanded: boolean;
+    isCollapsed: boolean;
     isPlaying: boolean;
     handlePlay: () => void;
     handleExpand: () => void;
@@ -12,7 +12,7 @@ type ListItemProps = {
 
 export function ListItem({
     data,
-    isExpanded,
+    isCollapsed,
     isPlaying,
     handlePlay,
     handleExpand,
@@ -20,19 +20,19 @@ export function ListItem({
 }: ListItemProps) {
     const i = {
         size: 20,
-        color: isExpanded
-            ? 'var(--c-amber-on-accent-container-primary)'
-            : 'var(--c-amber-accent-primary)'
+        color: isCollapsed
+            ? 'var(--c-amber-accent-primary)'
+            : 'var(--c-amber-on-accent-container-primary)'
     };
 
     const isPlayingClass = isPlaying ? s.isPlayingClass : '';
-    const isExpandedClass = isExpanded ? s.isExpandedClass : '';
+    const isCollapsedClass = isCollapsed ? s.isCollapsedClass : '';
 
     return (
-        <article className={`${s.outerWrapper} ${isExpandedClass}`}>
-            <div className={`${s.contentContainer} ${isPlayingClass}`}>
-                <header>
-                    <div className={s.clickableArea}>
+        <article className={`${s.outerWrapper} ${isCollapsedClass}`}>
+            <div className={`${s.contentContainer} ${isPlayingClass} ${isCollapsedClass}`}>
+                <header className={isCollapsedClass}>
+                    <div className={`${s.clickableArea} ${isCollapsedClass}`}>
                         <button
                             type='button'
                             className={s.expandButton}
@@ -41,20 +41,39 @@ export function ListItem({
                             <span>Expand item</span>
                         </button>
                         <div className={s.cardCover}>
+                            <Image src='/podcast-cover-placeholder-M.png' width={48} height={48} alt='Cover'/>
                         </div>
                         <h3>Faladev #28 | Por trás de barreiras e soluções propostas por micro-serviços</h3>
-                        <span className={isPlayingClass}>8 Jan 21 · Dego Fernandes, Dani Leão, Wesley Williams e Lucas Santos</span>
+                        <span className={`${isPlayingClass} ${isCollapsedClass}`}>8 Jan 21 · Dego Fernandes, Dani Leão, Wesley Williams e Lucas Santos</span>
                         <time className={isPlayingClass}>1:35:18</time>
                     </div>
-                    <button type='button' className={isExpanded ? s.playButtonPrimary : s.playButtonSecondary}>
-                        <Play width={i.size} height={i.size} color={i.color}/>
-                    </button>
+                    {
+                        isPlaying
+                        ? <div className={s.audioBarsWrapper}><AudioBars width={i.size} height={i.size} color='var(--c-amber-accent-primary)'/></div>
+                        : (
+                            <button
+                                type='button'
+                                className={isCollapsedClass ? s.playButtonSecondary : s.playButtonPrimary}
+                                onClick={handlePlay}
+                            >
+                                <Play width={i.size} height={i.size} color={i.color}/>
+                            </button>
+                        )
+                    }
+                    
                 </header>
                 <div className={s.bodyWrapper}>
                     <p>Nesse episódio do Faladev, Diego Fernandes se reúne com João Pedro Schmitz, Bruno Lemos e Diego Haz, para discutir sobre a importância da contribuição open source e quais desafios circulam na comunidade. 
 
 A gente passa a maior parte do tempo escrevendo código. Agora chegou o momento de falar sobre isso. Toda semana reunimos profissionais da tecnologia para discutir sobre tudo que circula na órbita da programação. O Faladev é um podcast original Rocketseat.</p>
-                    <button>See less</button>
+                    <button type='button' className={`${s.seeLessButton} ${isPlayingClass}`}>
+                        See less
+                        <ChevronUpMini
+                            height={10}
+                            width={10}
+                            color={isPlaying ? 'var(--c-amber-accent-primary)' : 'var(--c-on-surface-primary)'}
+                        />
+                    </button>
                 </div>
             </div>
         </article>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,65 @@
 import s from '../styles/home.module.scss';
 import { FeaturedContent } from "../components/FeaturedContent";
 import { ListItem } from "../components/ListItem";
+import { useState } from 'react';
+
+type f = {
+	id: number;
+	title: string;
+}
+
+const fData: f[] = [
+	{
+		id: 1,
+		title: 'hey'
+	},
+	{
+		id: 2,
+		title: 'hey'
+	},
+	{
+		id: 3,
+		title: 'hey'
+	},
+	{
+		id: 4,
+		title: 'hey'
+	},
+]
 
 export default function Home() {
-  return (
-      <main>
-        <h1 className="pageTitle">Amber podcasts</h1>
-        <FeaturedContent />
-        <section className={s.sectionContainer}>
-          <div className={s.contentContainer}>
-            <h2>All episodes · <span>16</span></h2>
-            <div className={s.contentListWrapper}>
-              <ListItem
-                data="hey"
-                isExpanded={false}
-                isPlaying={false}
-                handlePlay={() => console.log('hey there!')}
-                handleExpand={() => console.log('hey there!')}
-              />
-              <ListItem
-                data="hey"
-                isExpanded={false}
-                isPlaying={false}
-                handlePlay={() => console.log('hey there!')}
-                handleExpand={() => console.log('hey there!')}
-              />
-              <ListItem
-                data="hey"
-                isExpanded={true}
-                isPlaying={false}
-                handlePlay={() => console.log('hey there!')}
-                handleExpand={() => console.log('hey there!')}
-              />
-              <ListItem
-                data="hey"
-                isExpanded={false}
-                isPlaying={true}
-                handlePlay={() => console.log('hey there!')}
-                handleExpand={() => console.log('hey there!')}
-              />
-            </div>
-          </div>
-        </section>
-      </main>
-  )
+	const [isCollapsed, setIsCollapsed] = useState(true);
+	const [isPlaying, setIsPlaying] = useState(false);
+
+	function handleExpand() {
+		setIsCollapsed(!isCollapsed);
+	}
+
+	function handlePlay() {
+		setIsPlaying(!isPlaying);
+	}
+	
+	return (
+		<main>
+		<h1 className="pageTitle">Amber podcasts</h1>
+		<FeaturedContent />
+		<section className={s.sectionContainer}>
+			<div className={s.contentContainer}>
+			<h2>All episodes · <span>16</span></h2>
+			<div className={s.contentListWrapper}>
+				{fData.map((i) => (
+					<ListItem
+						key={i.id}
+						data="hey"
+						isCollapsed={isCollapsed}
+						isPlaying={isPlaying}
+						handlePlay={handlePlay}
+						handleExpand={handleExpand}
+					/>
+				))}
+			</div>
+			</div>
+		</section>
+		</main>
+	)
 }
