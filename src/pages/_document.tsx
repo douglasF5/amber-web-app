373cb748d2e5df0e4a73d2b580d99a733507dedb
Douglas Ferreira <dfaferreira46@gmail.com>
2022-07-31 17:24:22 -0300

    added favicon and project metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                <meta charSet="utf-8" />
+                    <meta name="author" content="Douglas Ferreira" />
+                    <meta name="robots" content="noindex, nofollow" />
+                    <link rel="icon" type="image/png" href="./favicon.png" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Lexend:wght@400;500;600;700&display=swap" rel="stylesheet" />
